38fb3b0e2881d3947eeb42dd8a7c149fc0182225
sqz777 <sqz777isme@gmail.com>
2017-12-19 23:42:54 +0800

    PASS ALL TEST CASE

--- Codewars_RemoveFirstandLastCharacter/Kata.cs
+++ Codewars_RemoveFirstandLastCharacter/Kata.cs
@@ -6,6 +6,8 @@ namespace Codewars_RemoveFirstandLastCharacter
     {
         public string Remove_char(string s)
         {
+            if (s.Length>1)
+                return s.Substring(1, s.Length - 2);
             return "ok";
         }
     }
--- Codewars_RemoveFirstandLastCharacter/UnitTest1.cs
+++ Codewars_RemoveFirstandLastCharacter/UnitTest1.cs
@@ -17,5 +17,28 @@ namespace Codewars_RemoveFirstandLastCharacter
             Assert.AreEqual("ok",new Kata().Remove_char("a"));
         }
 
+        [TestMethod]
+        public void Input_ab_Should_Be_StringEmpty()
+        {
+            Assert.AreEqual(string.Empty,new Kata().Remove_char("ab"));
+        }
+
+        [TestMethod]
+        public void Input_AacABvbSdkPfbDk_Should_Be_acABvbSdkPfbD()
+        {
+            Assert.AreEqual("acABvbSdkPfbD", new Kata().Remove_char("AacABvbSdkPfbDk"));
+        }
+
+        [TestMethod]
+        public void Input_ABC_Should_Be_B()
+        {
+            Assert.AreEqual("B", new Kata().Remove_char("ABC"));
+        }
+
+        [TestMethod]
+        public void Input_SpecialChars_Should_BeCuttenString()
+        {
+            Assert.AreEqual("!@##$%^$%^", new Kata().Remove_char("B!@##$%^$%^A"));
+        }
     }
 }

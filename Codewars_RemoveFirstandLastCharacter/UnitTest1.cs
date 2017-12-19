526e495272af4aabe93f46c3d7736dc1fbc34c19
sqz777 <sqz777isme@gmail.com>
2017-12-19 22:38:05 +0800

    加入專案檔案。

---
+++ Codewars_RemoveFirstandLastCharacter/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_RemoveFirstandLastCharacter
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
